d9e0400b04af0e6008bb45ed4221ade694ed3c15
sachin-kumar-cse <dev.sachin.suman@gmail.com>
2026-08-11 17:50:44 +0530

    feat(analytics): add Microsoft Clarity tracking code to head in app/layout.tsx and index.html

--- app/layout.tsx
+++ app/layout.tsx
@@ -323,6 +323,15 @@ export default function RootLayout({
           data-key="s4fMLQyoVzP9NLSXPjog5Q"
           strategy="lazyOnload"
         />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "y0pci80x1i");
+          `}
+        </Script>
         <Providers>
           <Layout>{children}</Layout>
         </Providers>
